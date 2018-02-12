e7a6b87da8739f406dcfd364cb5d15756806c8fd
Clifton Craig <clifton.craig@ge.com>
2018-02-12 13:24:02 -0800

    Completed exercise 1.

--- Exercise 1/Start/MyTunes.iOS/MyTunesViewController.cs
+++ Exercise 1/Start/MyTunes.iOS/MyTunesViewController.cs
@@ -1,4 +1,5 @@
 using UIKit;
+using System.Linq;
 
 namespace MyTunes
 {
@@ -11,13 +12,24 @@ namespace MyTunes
 			TableView.ContentInset = new UIEdgeInsets (20, 0, 0, 0);
 		}
 
-		public override void ViewDidLoad()
+		public async override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
 
-			TableView.Source = new ViewControllerSource<string>(TableView) {
-				DataSource = new string[] { "One", "Two", "Three" },
-			};
+            //TableView.Source = new ViewControllerSource<string>(TableView) {
+            //	DataSource = new string[] { "One", "Two", "Three" },
+            //};
+
+            // Load the data
+            var data = await SongLoader.Load();
+
+            // Register the TableView's data source
+            TableView.Source = new ViewControllerSource<Song>(TableView)
+            {
+                DataSource = data.ToList(),
+                TextProc = s => s.Name,
+                DetailTextProc = s => s.Artist + " - " + s.Album,
+            };
 		}
 	}
 

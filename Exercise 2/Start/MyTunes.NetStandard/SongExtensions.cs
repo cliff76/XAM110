a9c261f39fe172f396494a3a3a24e67d6c95375f
Clifton Craig <clifton.craig@gmail.com>
2018-02-13 13:52:38 -0800

    Completes exercise 4

--- Exercise 2/Start/MyTunes.NetStandard/SongExtensions.cs
+++ Exercise 2/Start/MyTunes.NetStandard/SongExtensions.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+//using System.Net.Http;
 
 namespace MyTunes
 {
     public static class SongExtensions
     {
-        static HttpClient httpClient = new HttpClient();
+        //static HttpClient httpClient = new HttpClient();
 
         public static string RuinSongName(this string songname) {
             return songname.Replace("Crocodile", "Alligator").Replace("You", "Me").Replace("the", "our").Replace("The", "Your");
